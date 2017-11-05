3f3e212be1c92d79368dc1b100f5e149a3d3e082
John <jonnygovish@gmail.com>
2017-11-05 11:42:31 +0300

    configuration for simplemde

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 class Config:
 	SECRET_KEY=os.environ.get('SECRET_KEY')
 	SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://puppah:zanpakutou@localhost/blogworld'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False	
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	# simple mde  configurations
+	SIMPLEMDE_JS_IIFE = True
+	SIMPLEMDE_USE_CDN = True	
 
   
 
