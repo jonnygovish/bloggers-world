1004ce351e167e0d20b312c0c3f01a536ad67846
John <jonnygovish@gmail.com>
2017-11-08 03:45:33 +0300

    refactoring

--- app/main/views.py
+++ app/main/views.py
@@ -14,15 +14,17 @@ def index():
   title ="Blogs World"
   return render_template('index.html', title=title,blogs=blogs)
 
-@main.route('/admin/dashboard')
-@login_required
-def admin_dashboard():
-  #prevents non-admin from accessing the page
-  if not current_user.is_admin:
-    abort(403)
+# @main.route('/admin/dashboard')
+# @login_required
+# def admin_dashboard():
+#   #prevents non-admin from accessing the page
+#   if not current_user.is_admin:
+#     abort(403)
+#   else:
+#     blogs = Blog.get_blog()
 
-  title = 'Dashboard'
-  return render_template('admin/admin_dashboard.html',title = title)
+#   title = 'Dashboard'
+#   return render_template('admin/admin_dashboard.html',title = title,blogs =blogs)
 
 @main.route('/blog/<int:id>')
 def blog(id):
@@ -32,6 +34,7 @@ def blog(id):
   title ="Blogs"
 
   return render_template('blog.html', title =title, blogs=blogs,comments=comments,comment_form= form)
+
 
 
 @main.route('/blog/<int:id>',methods=["GET","POST"])
@@ -49,4 +52,33 @@ def new_comment(id):
     return redirect(url_for('.blog', id = blog.id))
 
   return render_template('blog.html', comment_form =form)
+
+@main.route('/blog/comment/<int:id>')
+def single_comment(id):
+  
+  comment = Comment.query.get(id)
+
+  if comment is None:
+    abort(404)
+
+  format_comment = markdown2.markdown(comment.content,extras=["code-friendly", "fenced-code-blocks"])
+  return render_template('comment.html', comment= comment, format_comment= format_comment)
+
+@main.route('/blog/comment/delete/<int:id>')
+def delete_comment(id):
+  
+  comment = Comment.query.get(id)
+  comment.delete_comments(id)
+
+  return redirect(url_for('.index'))
+
+@main.route('/blog/delete/<int:id>')
+def delete_blog(id):
+  
+  blog = Blog.query.get(id)
+  blog.delete_blog(id)
+
+  return redirect(url_for('.index'))
+
+  
 
